59dd1e6453cdfd4809b1843e90aa21acd96f490f
Wes House <wesevan7@gmail.com>
2021-03-25 12:09:56 +0100

    changed ssh file

--- setup.sh
+++ setup.sh
@@ -1,13 +1,21 @@
+# mkdir -p ~/.streamlit/
+
+# echo "\
+# [general]\n\
+# email = \"${wesevan7@gmail.com}\"\n\
+# " > ~/.streamlit/credentials.toml
+
+# echo "\
+# [server]\n\
+# headless = true\n\
+# enableCORS = false\n\
+# port = $PORT\n\
+# " > ~/.streamlit/config.toml
+
 mkdir -p ~/.streamlit/
-
-echo "\
-[general]\n\
-email = \"${wesevan7@gmail.com}\"\n\
-" > ~/.streamlit/credentials.toml
-
-echo "\
-[server]\n\
-headless = true\n\
-enableCORS = false\n\
-port = $PORT\n\
-" > ~/.streamlit/config.toml
+echo “
+[server]
+headless = true
+enableCORS=false
+port = $PORT
+“ > ~/.streamlit/config.toml
