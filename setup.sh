932589327a0f8660b8c2cd3efb7282f1dd9e943d
Wes House <wesevan7@gmail.com>
2021-03-25 12:21:49 +0100

    reverted ssh file and added nltk.txt

--- setup.sh
+++ setup.sh
@@ -1,21 +1,14 @@
-# mkdir -p ~/.streamlit/
-
-# echo "\
-# [general]\n\
-# email = \"${wesevan7@gmail.com}\"\n\
-# " > ~/.streamlit/credentials.toml
-
-# echo "\
-# [server]\n\
-# headless = true\n\
-# enableCORS = false\n\
-# port = $PORT\n\
-# " > ~/.streamlit/config.toml
-
 mkdir -p ~/.streamlit/
-echo “
-[server]
-headless = true
-enableCORS=false
-port = $PORT
-“ > ~/.streamlit/config.toml
+
+echo "\
+[general]\n\
+email = \"${wesevan7@gmail.com}\"\n\
+" > ~/.streamlit/credentials.toml
+
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS = false\n\
+port = $PORT\n\
+" > ~/.streamlit/config.toml
+
